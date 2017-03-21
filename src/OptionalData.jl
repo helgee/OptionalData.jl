3ca229f7f40b90360d1de4a62f17dfa30d446169
Helge Eichhorn <git@helgeeichhorn.de>
2017-03-21 12:49:41 +0100

    Initial sketch.

--- src/OptionalData.jl
+++ src/OptionalData.jl
@@ -1,5 +1,24 @@
 module OptionalData
 
-# package code goes here
+export OptData, push!, isavailable
+
+import Base: push!
+
+mutable struct OptData{T}
+    data::Nullable{T}
+    OptData(::Type{T}) where T = new{T}(nothing)
+end
+
+function push!(opt::OptData{T}, data) where T
+    opt.data = Nullable{T}(data)
+    opt
+end
+
+function push!(opt::OptData{T}, args...) where T
+    push!(opt, T(args...))
+    opt
+end
+
+isavailable(opt::OptData) = isnull(opt.data)
 
 end # module
