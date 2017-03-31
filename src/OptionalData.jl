b936458a0cbbde203e7511940c975e9d4e5af2f0
Helge Eichhorn <git@helgeeichhorn.de>
2017-03-31 11:10:26 +0200

    Add docstrings

--- src/OptionalData.jl
+++ src/OptionalData.jl
@@ -18,26 +18,59 @@ function show{T}(io::IO, opt::OptData{T})
     print(io, "OptData{$T}($val)")
 end
 
+"""
+    push!{T}(opt::OptData{T}, data::T)
+
+Push `data` of type `T` to `opt`.
+"""
 function push!{T}(opt::OptData{T}, data::T)
     opt.data = Nullable{T}(data)
     opt
 end
 
+"""
+    push!{T}(opt::OptData, ::Type{T}, args...)
+
+Construct an object of type `T` from `args` and push it to `opt`.
+"""
 function push!{T}(opt::OptData, ::Type{T}, args...)
     push!(opt, T(args...))
     opt
 end
 push!{T}(opt::OptData{T}, args...) = push!(opt, T, args...)
 
+"""
+    get(opt::OptData)
+
+Get data from `opt`. Throw an exception if no data has been pushed to `opt` before.
+"""
 function get(opt::OptData)
     !isavailable(opt) && error(opt.name, " is not available. ", opt.msg)
     get(opt.data)
 end
 
+"""
+    @OptionalData name type msg=""
+
+Initialise a constant `name` with type `OptData{type}`.
+An exception with the custom error message `msg` is thrown when `name` is
+accessed before data has been pushed to it.
+
+# Example
+
+```julia
+@OptionalData OPT_FLOAT Float64
+```
+"""
 macro OptionalData(name, typ, msg="")
    :(const $(esc(name)) = OptData($(esc(typ)), $(string(name)), $msg))
 end
 
+"""
+    isavailable(opt::OptData)
+
+Check whether data has been pushed to `opt`.
+"""
 isavailable(opt::OptData) = !isnull(opt.data)
 
 end # module
